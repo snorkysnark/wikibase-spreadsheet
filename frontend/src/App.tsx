59d059ae2bd129819940567ecb259ba1ca423d40
snorkysnark <snorkysnark@gmail.com>
2024-02-26 14:08:42 +0100

    table library example

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { HotTable } from "@handsontable/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <HotTable
+        data={[
+          ["", "Tesla", "Volvo", "Toyota", "Ford"],
+          ["2019", 10, 11, 12, 13],
+          ["2020", 20, 11, 14, 13],
+          ["2021", 30, 15, 12, 13],
+        ]}
+        rowHeaders={true}
+        colHeaders={true}
+        height="auto"
+        autoWrapRow={true}
+        autoWrapCol={true}
+        licenseKey="non-commercial-and-evaluation" // for non-commercial use only
+      />
     </>
   );
 }
